76c19f92857105bc4b46ff618468b620f52e7ba7
davidliu-github <davidliu@davidliugram>
2024-03-18 22:44:09 -0400

    a4 state and redux fa23

--- src/Labs/a3/index.tsx
+++ src/Labs/a3/index.tsx
@@ -1,3 +1,5 @@
+import { useSelector } from "react-redux";
+import { LabState } from "../store";
 import Classes from "./Classes";
 import Styles from "./Styles"
 import ConditionalOutput from "./ConditionalOutput";
@@ -8,11 +10,18 @@ import TodoList from "./todos/TodoList";
 import JavaScript from "./JavaScript";
 import PathParameters from "./routing/PathParameters";;
 
-
 function Assignment3() {
+   const { todos } = useSelector((state: LabState) => state.todosReducer);
  return (
    <div>
      <h1>Assignment 3</h1>
+     <ul className="list-group">
+        {todos.map((todo) => (
+          <li className="list-group-item" key={todo.id}>
+            {todo.title}
+          </li>
+        ))}
+      </ul>
      <ConditionalOutput/>
      <Styles/>
      <Classes/>
